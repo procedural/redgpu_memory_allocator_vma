efad169c8f44aac5d7c6651b6c1fcffc731102c7
Constantine Tarasenkov <iamvfx@gmail.com>
2021-10-04 21:21:42 +0300

    Add VMA to procedure names

--- redgpu_memory_allocator_functions.cpp
+++ redgpu_memory_allocator_functions.cpp
@@ -145,7 +145,7 @@ typedef struct RmaPhysicalDeviceProperties {
   RmaPhysicalDeviceSparseProperties _6;
 } RmaPhysicalDeviceProperties;
 
-REDGPU_DECLSPEC void REDGPU_API rmaVkGetPhysicalDeviceProperties(RedContext context, unsigned gpuIndex, RedHandleGpuDevice physicalDevice, void * pVkPhysicalDeviceProperties) {
+REDGPU_DECLSPEC void REDGPU_API rmaVmaVkGetPhysicalDeviceProperties(RedContext context, unsigned gpuIndex, RedHandleGpuDevice physicalDevice, void * pVkPhysicalDeviceProperties) {
   RmaPhysicalDeviceProperties properties = {};
   properties.deviceType                      = context->gpus[gpuIndex].gpuType;
   properties.limits.maxMemoryAllocationCount = context->gpus[gpuIndex].maxMemoryAllocateCount;
@@ -155,7 +155,7 @@ REDGPU_DECLSPEC void REDGPU_API rmaVkGetPhysicalDeviceProperties(RedContext cont
   out[0] = properties;
 }
 
-REDGPU_DECLSPEC void REDGPU_API rmaVkGetPhysicalDeviceMemoryProperties(RedContext context, unsigned gpuIndex, RedHandleGpuDevice physicalDevice, void * pVkPhysicalDeviceMemoryProperties) {
+REDGPU_DECLSPEC void REDGPU_API rmaVmaVkGetPhysicalDeviceMemoryProperties(RedContext context, unsigned gpuIndex, RedHandleGpuDevice physicalDevice, void * pVkPhysicalDeviceMemoryProperties) {
   VkPhysicalDeviceMemoryProperties properties = {};
   properties.memoryTypeCount = context->gpus[gpuIndex].memoryTypesCount;
   properties.memoryHeapCount = context->gpus[gpuIndex].memoryHeapsCount;
@@ -186,7 +186,7 @@ REDGPU_DECLSPEC void REDGPU_API rmaVkGetPhysicalDeviceMemoryProperties(RedContex
   out[0] = properties;
 }
 
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkAllocateMemory(RedContext context, unsigned gpuIndex, RedHandleGpu device, const void * pVkMemoryAllocateInfo, const void * pVkAllocationCallbacks, RedHandleMemory * pMemory) {
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkAllocateMemory(RedContext context, unsigned gpuIndex, RedHandleGpu device, const void * pVkMemoryAllocateInfo, const void * pVkAllocationCallbacks, RedHandleMemory * pMemory) {
   const VkMemoryAllocateInfo * memoryAllocateInfo = (const VkMemoryAllocateInfo *)pVkMemoryAllocateInfo;
   RedStatuses statuses = {};
   if (context->gpus[gpuIndex].memoryTypes[memoryAllocateInfo->memoryTypeIndex].isCpuMappable == 1) {
@@ -199,33 +199,33 @@ REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkAllocateMemory(RedContext context, uns
   return statuses.statusError;
 }
 
-REDGPU_DECLSPEC void REDGPU_API rmaVkFreeMemory(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleMemory memory, const void * pVkAllocationCallbacks) {
+REDGPU_DECLSPEC void REDGPU_API rmaVmaVkFreeMemory(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleMemory memory, const void * pVkAllocationCallbacks) {
   redMemoryFree(context, device, memory, 0, 0, 0);
 }
 
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkMapMemory(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleMemory memory, uint64_t offset, uint64_t size, unsigned vkMemoryMapFlags, void ** ppData) {
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkMapMemory(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleMemory memory, uint64_t offset, uint64_t size, unsigned vkMemoryMapFlags, void ** ppData) {
   RedStatuses statuses = {};
   redMemoryMap(context, device, memory, offset, size, ppData, &statuses, 0, 0, 0);
   return statuses.statusError;
 }
 
-REDGPU_DECLSPEC void REDGPU_API rmaVkUnmapMemory(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleMemory memory) {
+REDGPU_DECLSPEC void REDGPU_API rmaVmaVkUnmapMemory(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleMemory memory) {
   redMemoryUnmap(context, device, memory, 0, 0, 0);
 }
 
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkFlushMappedMemoryRanges(RedContext context, unsigned gpuIndex, RedHandleGpu device, unsigned memoryRangeCount, const void * pVkMappedMemoryRange) {
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkFlushMappedMemoryRanges(RedContext context, unsigned gpuIndex, RedHandleGpu device, unsigned memoryRangeCount, const void * pVkMappedMemoryRange) {
   RedStatuses statuses = {};
   redMemoryNonCoherentFlush(context, device, memoryRangeCount, (const RedMappableMemoryRange *)pVkMappedMemoryRange, &statuses, 0, 0, 0);
   return statuses.statusError;
 }
 
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkInvalidateMappedMemoryRanges(RedContext context, unsigned gpuIndex, RedHandleGpu device, unsigned memoryRangeCount, const void * pVkMappedMemoryRange) {
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkInvalidateMappedMemoryRanges(RedContext context, unsigned gpuIndex, RedHandleGpu device, unsigned memoryRangeCount, const void * pVkMappedMemoryRange) {
   RedStatuses statuses = {};
   redMemoryNonCoherentInvalidate(context, device, memoryRangeCount, (const RedMappableMemoryRange *)pVkMappedMemoryRange, &statuses, 0, 0, 0);
   return statuses.statusError;
 }
 
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkBindBufferMemory(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleArray buffer, RedHandleMemory memory, uint64_t memoryOffset) {
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkBindBufferMemory(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleArray buffer, RedHandleMemory memory, uint64_t memoryOffset) {
   RedStatuses statuses = {};
   RedMemoryArray memoryArray = {};
   memoryArray.setTo1000157000  = 1000157000;
@@ -237,7 +237,7 @@ REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkBindBufferMemory(RedContext context, u
   return statuses.statusError;
 }
 
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkBindImageMemory(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleImage image, RedHandleMemory memory, uint64_t memoryOffset) {
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkBindImageMemory(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleImage image, RedHandleMemory memory, uint64_t memoryOffset) {
   RedStatuses statuses = {};
   RedMemoryImage memoryImage = {};
   memoryImage.setTo1000157001  = 1000157001;
@@ -249,7 +249,7 @@ REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkBindImageMemory(RedContext context, un
   return statuses.statusError;
 }
 
-REDGPU_DECLSPEC void REDGPU_API rmaVkGetBufferMemoryRequirements(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleArray buffer, void * pVkMemoryRequirements) {
+REDGPU_DECLSPEC void REDGPU_API rmaVmaVkGetBufferMemoryRequirements(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleArray buffer, void * pVkMemoryRequirements) {
   RedArray arrayData = __REDGPU_MEMORY_ALLOCATOR_FUNCTIONS_GLOBAL_ec8b2cdda35a8068bba6ef47ad511ac00d5c39d6_mapArrays[buffer];
   VkMemoryRequirements * out = (VkMemoryRequirements *)pVkMemoryRequirements;
   out->size           = arrayData.memoryBytesCount;
@@ -257,7 +257,7 @@ REDGPU_DECLSPEC void REDGPU_API rmaVkGetBufferMemoryRequirements(RedContext cont
   out->memoryTypeBits = arrayData.memoryTypesSupported;
 }
 
-REDGPU_DECLSPEC void REDGPU_API rmaVkGetImageMemoryRequirements(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleImage image, void * pVkMemoryRequirements) {
+REDGPU_DECLSPEC void REDGPU_API rmaVmaVkGetImageMemoryRequirements(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleImage image, void * pVkMemoryRequirements) {
   RedImage imageData = __REDGPU_MEMORY_ALLOCATOR_FUNCTIONS_GLOBAL_ec8b2cdda35a8068bba6ef47ad511ac00d5c39d6_mapImages[image];
   VkMemoryRequirements * out = (VkMemoryRequirements *)pVkMemoryRequirements;
   out->size           = imageData.memoryBytesCount;
@@ -265,7 +265,7 @@ REDGPU_DECLSPEC void REDGPU_API rmaVkGetImageMemoryRequirements(RedContext conte
   out->memoryTypeBits = imageData.memoryTypesSupported;
 }
 
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkCreateBuffer(RedContext context, unsigned gpuIndex, RedHandleGpu device, const void * pVkBufferCreateInfo, const void * pVkAllocationCallbacks, RedHandleArray * pBuffer) {
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkCreateBuffer(RedContext context, unsigned gpuIndex, RedHandleGpu device, const void * pVkBufferCreateInfo, const void * pVkAllocationCallbacks, RedHandleArray * pBuffer) {
   const VkBufferCreateInfo * createInfo = (const VkBufferCreateInfo *)pVkBufferCreateInfo;
   RedStatuses statuses = {};
   // NOTE(Constantine): Pass structuredBufferElementBytesCount from VMA to RMA in future.
@@ -282,7 +282,7 @@ REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkCreateBuffer(RedContext context, unsig
   return statuses.statusError;
 }
 
-REDGPU_DECLSPEC void REDGPU_API rmaVkDestroyBuffer(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleArray buffer, const void * pVkAllocationCallbacks) {
+REDGPU_DECLSPEC void REDGPU_API rmaVmaVkDestroyBuffer(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleArray buffer, const void * pVkAllocationCallbacks) {
   redDestroyArray(context, device, buffer, 0, 0, 0);
   {
     std::lock_guard<std::mutex> __mapArraysMutexScope(__REDGPU_MEMORY_ALLOCATOR_FUNCTIONS_GLOBAL_ec8b2cdda35a8068bba6ef47ad511ac00d5c39d6_mapArraysMutex);
@@ -290,7 +290,7 @@ REDGPU_DECLSPEC void REDGPU_API rmaVkDestroyBuffer(RedContext context, unsigned 
   }
 }
 
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkCreateImage(RedContext context, unsigned gpuIndex, RedHandleGpu device, const void * pVkImageCreateInfo, const void * pVkAllocationCallbacks, RedHandleImage * pImage) {
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkCreateImage(RedContext context, unsigned gpuIndex, RedHandleGpu device, const void * pVkImageCreateInfo, const void * pVkAllocationCallbacks, RedHandleImage * pImage) {
   const VkImageCreateInfo * createInfo = (const VkImageCreateInfo *)pVkImageCreateInfo;
   RedStatuses statuses = {};
   RedImageDimensions imageDimensions = RED_IMAGE_DIMENSIONS_2D;
@@ -341,7 +341,7 @@ REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkCreateImage(RedContext context, unsign
   return statuses.statusError;
 }
 
-REDGPU_DECLSPEC void REDGPU_API rmaVkDestroyImage(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleImage image, const void * pVkAllocationCallbacks) {
+REDGPU_DECLSPEC void REDGPU_API rmaVmaVkDestroyImage(RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleImage image, const void * pVkAllocationCallbacks) {
   redDestroyImage(context, device, image, 0, 0, 0);
   {
     std::lock_guard<std::mutex> __mapImagesMutexScope(__REDGPU_MEMORY_ALLOCATOR_FUNCTIONS_GLOBAL_ec8b2cdda35a8068bba6ef47ad511ac00d5c39d6_mapImagesMutex);
@@ -349,7 +349,7 @@ REDGPU_DECLSPEC void REDGPU_API rmaVkDestroyImage(RedContext context, unsigned g
   }
 }
 
-REDGPU_DECLSPEC void REDGPU_API rmaVkCmdCopyBuffer(RedContext context, unsigned gpuIndex, RedHandleCalls commandBuffer, RedHandleArray srcBuffer, RedHandleArray dstBuffer, unsigned regionCount, const void * pVkBufferCopy) {
+REDGPU_DECLSPEC void REDGPU_API rmaVmaVkCmdCopyBuffer(RedContext context, unsigned gpuIndex, RedHandleCalls commandBuffer, RedHandleArray srcBuffer, RedHandleArray dstBuffer, unsigned regionCount, const void * pVkBufferCopy) {
   RedCallProceduresAndAddresses callPAs = {};
   redGetCallProceduresAndAddresses(context, context->gpus[gpuIndex].gpu, &callPAs, 0, 0, 0, 0);
   callPAs.redCallCopyArrayToArray(commandBuffer, srcBuffer, dstBuffer, regionCount, (const RedCopyArrayRange *)pVkBufferCopy);
--- redgpu_memory_allocator_functions.h
+++ redgpu_memory_allocator_functions.h
@@ -11,23 +11,23 @@
 extern "C" {
 #endif
 
-REDGPU_DECLSPEC void      REDGPU_API rmaVkGetPhysicalDeviceProperties       (RedContext context, unsigned gpuIndex, RedHandleGpuDevice physicalDevice, void * pVkPhysicalDeviceProperties);
-REDGPU_DECLSPEC void      REDGPU_API rmaVkGetPhysicalDeviceMemoryProperties (RedContext context, unsigned gpuIndex, RedHandleGpuDevice physicalDevice, void * pVkPhysicalDeviceMemoryProperties);
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkAllocateMemory                    (RedContext context, unsigned gpuIndex, RedHandleGpu device, const void * pVkMemoryAllocateInfo, const void * pVkAllocationCallbacks, RedHandleMemory * pMemory);
-REDGPU_DECLSPEC void      REDGPU_API rmaVkFreeMemory                        (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleMemory memory, const void * pVkAllocationCallbacks);
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkMapMemory                         (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleMemory memory, uint64_t offset, uint64_t size, unsigned vkMemoryMapFlags, void ** ppData);
-REDGPU_DECLSPEC void      REDGPU_API rmaVkUnmapMemory                       (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleMemory memory);
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkFlushMappedMemoryRanges           (RedContext context, unsigned gpuIndex, RedHandleGpu device, unsigned memoryRangeCount, const void * pVkMappedMemoryRange);
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkInvalidateMappedMemoryRanges      (RedContext context, unsigned gpuIndex, RedHandleGpu device, unsigned memoryRangeCount, const void * pVkMappedMemoryRange);
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkBindBufferMemory                  (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleArray buffer, RedHandleMemory memory, uint64_t memoryOffset);
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkBindImageMemory                   (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleImage image, RedHandleMemory memory, uint64_t memoryOffset);
-REDGPU_DECLSPEC void      REDGPU_API rmaVkGetBufferMemoryRequirements       (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleArray buffer, void * pVkMemoryRequirements);
-REDGPU_DECLSPEC void      REDGPU_API rmaVkGetImageMemoryRequirements        (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleImage image, void * pVkMemoryRequirements);
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkCreateBuffer                      (RedContext context, unsigned gpuIndex, RedHandleGpu device, const void * pVkBufferCreateInfo, const void * pVkAllocationCallbacks, RedHandleArray * pBuffer);
-REDGPU_DECLSPEC void      REDGPU_API rmaVkDestroyBuffer                     (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleArray buffer, const void * pVkAllocationCallbacks);
-REDGPU_DECLSPEC RedStatus REDGPU_API rmaVkCreateImage                       (RedContext context, unsigned gpuIndex, RedHandleGpu device, const void * pVkImageCreateInfo, const void * pVkAllocationCallbacks, RedHandleImage * pImage);
-REDGPU_DECLSPEC void      REDGPU_API rmaVkDestroyImage                      (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleImage image, const void * pVkAllocationCallbacks);
-REDGPU_DECLSPEC void      REDGPU_API rmaVkCmdCopyBuffer                     (RedContext context, unsigned gpuIndex, RedHandleCalls commandBuffer, RedHandleArray srcBuffer, RedHandleArray dstBuffer, unsigned regionCount, const void * pVkBufferCopy);
+REDGPU_DECLSPEC void      REDGPU_API rmaVmaVkGetPhysicalDeviceProperties       (RedContext context, unsigned gpuIndex, RedHandleGpuDevice physicalDevice, void * pVkPhysicalDeviceProperties);
+REDGPU_DECLSPEC void      REDGPU_API rmaVmaVkGetPhysicalDeviceMemoryProperties (RedContext context, unsigned gpuIndex, RedHandleGpuDevice physicalDevice, void * pVkPhysicalDeviceMemoryProperties);
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkAllocateMemory                    (RedContext context, unsigned gpuIndex, RedHandleGpu device, const void * pVkMemoryAllocateInfo, const void * pVkAllocationCallbacks, RedHandleMemory * pMemory);
+REDGPU_DECLSPEC void      REDGPU_API rmaVmaVkFreeMemory                        (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleMemory memory, const void * pVkAllocationCallbacks);
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkMapMemory                         (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleMemory memory, uint64_t offset, uint64_t size, unsigned vkMemoryMapFlags, void ** ppData);
+REDGPU_DECLSPEC void      REDGPU_API rmaVmaVkUnmapMemory                       (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleMemory memory);
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkFlushMappedMemoryRanges           (RedContext context, unsigned gpuIndex, RedHandleGpu device, unsigned memoryRangeCount, const void * pVkMappedMemoryRange);
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkInvalidateMappedMemoryRanges      (RedContext context, unsigned gpuIndex, RedHandleGpu device, unsigned memoryRangeCount, const void * pVkMappedMemoryRange);
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkBindBufferMemory                  (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleArray buffer, RedHandleMemory memory, uint64_t memoryOffset);
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkBindImageMemory                   (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleImage image, RedHandleMemory memory, uint64_t memoryOffset);
+REDGPU_DECLSPEC void      REDGPU_API rmaVmaVkGetBufferMemoryRequirements       (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleArray buffer, void * pVkMemoryRequirements);
+REDGPU_DECLSPEC void      REDGPU_API rmaVmaVkGetImageMemoryRequirements        (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleImage image, void * pVkMemoryRequirements);
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkCreateBuffer                      (RedContext context, unsigned gpuIndex, RedHandleGpu device, const void * pVkBufferCreateInfo, const void * pVkAllocationCallbacks, RedHandleArray * pBuffer);
+REDGPU_DECLSPEC void      REDGPU_API rmaVmaVkDestroyBuffer                     (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleArray buffer, const void * pVkAllocationCallbacks);
+REDGPU_DECLSPEC RedStatus REDGPU_API rmaVmaVkCreateImage                       (RedContext context, unsigned gpuIndex, RedHandleGpu device, const void * pVkImageCreateInfo, const void * pVkAllocationCallbacks, RedHandleImage * pImage);
+REDGPU_DECLSPEC void      REDGPU_API rmaVmaVkDestroyImage                      (RedContext context, unsigned gpuIndex, RedHandleGpu device, RedHandleImage image, const void * pVkAllocationCallbacks);
+REDGPU_DECLSPEC void      REDGPU_API rmaVmaVkCmdCopyBuffer                     (RedContext context, unsigned gpuIndex, RedHandleCalls commandBuffer, RedHandleArray srcBuffer, RedHandleArray dstBuffer, unsigned regionCount, const void * pVkBufferCopy);
 
 #ifdef __cplusplus
 }
